1ab17ceb93406151832e2806b2e1a60642359e44
Yago <yagolopeslazaro@gmail.com>
2019-03-22 15:12:29 -0300

    inicial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+export default function App() {
+  const [location, setLocation] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.watchPosition(handlePositionReceived);
+  }, []);
+
+  function handlePositionReceived({ coords }) {
+    const { latitude, longitude } = coords;
+    setLocation({ latitude, longitude });
+  }
+
+  return (
+    <>
+      Latitude: {location.latitude} <br />
+      Longitude: {location.longitude}
+    </>
+  );
+}
